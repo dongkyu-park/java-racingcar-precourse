1363a3d9a02836f01eb81136a6551dbc7fe88da2
dongkyu <dongkyu.parkk@gmail.com>
2021-05-02 16:21:11 +0900

    feat(Car->Cars, CarTest->CarsTest): 클래스명 수정 및 로직 추가
    사용자 입력값 중복 검증 로직 추가
    사용자 입력값 공백 없애기 로직 추가
    클래스명 Car -> Cars 로 변경

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,53 @@
+package racingcar;
+
+import java.util.*;
+
+public class Cars {
+    public static final int MIN_LENGTH = 1;
+    public static final int MAX_LENGTH = 5;
+    public static final int MIN_CAR_ARRAY = 2;
+
+    private int carNameMinLength = MIN_LENGTH;
+    private int carNameMaxLength = MAX_LENGTH;
+    private List<Car> carList;
+
+    public Cars(String car) {
+        String[] tempCarArray = car.replaceAll(" ", "").split(",");
+        if (!validCarNameCount(tempCarArray)) {
+            throw new IllegalArgumentException("레이싱 카의 이름은 1자 이상, 5자 이하로 입력해야 합니다.");
+        }
+        if (!validCarArrayCount(tempCarArray)) {
+            throw new IllegalArgumentException("2개 이상의 레이싱 카를 입력해야 합니다.");
+        }
+        if (!validCarNameDuplicate(tempCarArray)) {
+            throw new IllegalArgumentException("중복 된 레이싱 카 이름이 존재합니다.");
+        }
+        this.carList = mapCar(tempCarArray);
+    }
+
+    private List<Car> mapCar(String[] tempCarArray) {
+        List<Car> carList = new ArrayList<>();
+        for (String car : tempCarArray) {
+            carList.add(new Car(car));
+        }
+        return carList;
+    }
+
+    private boolean validCarNameDuplicate(String[] tempCarArray) {
+        Set<String> nonDuplicateCarName = new HashSet<>(Arrays.asList(tempCarArray));
+        return nonDuplicateCarName.size() == tempCarArray.length;
+    }
+
+    private boolean validCarArrayCount(String[] tempCarArray) {
+        return tempCarArray.length >= MIN_CAR_ARRAY;
+    }
+
+    public boolean validCarNameCount(String[] tempCarArray) {
+        for (String car : tempCarArray) {
+            carNameMinLength = Math.min(carNameMinLength, car.length());
+            carNameMaxLength = Math.max(carNameMaxLength, car.length());
+        }
+        return carNameMinLength >= MIN_LENGTH && carNameMaxLength <= MAX_LENGTH;
+    }
+
+}
---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+public class CarsTest {
+
+    @Test
+    void validate_car_name_1_5() {
+        assertThatThrownBy(()->new Cars("t, test2,, test")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->new Cars("test1, 6글자테스트, test5, test")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_car_min_count_2() {
+        assertThatThrownBy(()->new Cars("")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->new Cars("test")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_car_name_duplicate() {
+        assertThatThrownBy(()->new Cars("test, test")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
