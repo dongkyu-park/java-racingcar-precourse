5d4dff33e4e7e2cea954c12e146b40fb7dccc731
dongkyu <dongkyu.parkk@gmail.com>
2021-05-04 19:35:02 +0900

    refactor: 하드코딩한 값 상수로 변경

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,15 +1,19 @@
 package racingcar;
 
+import javax.xml.bind.annotation.XmlType;
 import java.util.List;
 
 public class Game {
+
+    public static final int DO_GO = 4;
+    public static final int DEFAULT_POSITION = 0;
 
     public int generatePower() {
         return (int)(Math.random()*10);
     }
 
     public CarStatus setCarStatus(int power) {
-        if (power >= 4) {
+        if (power >= DO_GO) {
             return CarStatus.GO;
         }
         return CarStatus.STOP;
@@ -28,7 +32,7 @@ public class Game {
     }
 
     private int getMaxPosition(List<Car> result) {
-        int position = 0;
+        int position = DEFAULT_POSITION;
         for (int i = 0; i < result.size(); i++) {
             position = Math.max(position, result.get(i).getPosition());
         }
